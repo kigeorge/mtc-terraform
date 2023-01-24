199886d552493fd698e842b4ede2202acb0517d0
king <kingtamgeorge@gmail.com>
2023-01-24 07:31:32 -0800

    Remove

--- king-tf-docker-basics/output.tf
+++
@@ -1,9 +0,0 @@
-output "ip_address" {
-  value       = [for i in docker_container.nodered_container[*]: join(":", [i.ip_address],i.ports[*]["external"])]
-  description = "The IP address of the container"
-}
-
-output "container-name" {
-  value       = docker_container.nodered_container[*].name
-  description = "The name of container"
-}
